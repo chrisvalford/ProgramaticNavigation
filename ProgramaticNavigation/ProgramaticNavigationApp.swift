8113a8d171ae783c260c41704dbaeb674914f010
Christopher Alford <chrisvalford@me.com>
2023-09-26 19:03:41 +0200

    Initial Commit

---
+++ ProgramaticNavigation/ProgramaticNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgramaticNavigationApp.swift
+//  ProgramaticNavigation
+//
+//  Created by Christopher Alford on 26/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgramaticNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
